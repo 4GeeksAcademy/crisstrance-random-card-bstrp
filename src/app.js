81eed40dd70df530482b0bb27910263f9ecf9c09
Cristian Aravena <50273105+crisstrance@users.noreply.github.com>
2024-08-12 17:30:11 +0000

    random card

--- src/app.js
+++ src/app.js
@@ -2,10 +2,41 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function newCard() {
+  let suite = ["♦", "♥", "♠", "♣"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let indexSuite = Math.floor(Math.random() * suite.length);
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  document.querySelector("#top").innerHTML = suite[indexSuite];
+  document.querySelector("#number").innerHTML = numbers[indexNumbers];
+  document.querySelector("#end").innerHTML = suite[indexSuite];
+
+  let suiteColor =
+    suite[indexSuite] == "♦" || suite[indexSuite] == "♥"
+      ? "text-danger"
+      : "text-dark";
+  document.querySelector("#top").className = suiteColor;
+  document.querySelector("#end").className = suiteColor;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  newCard();
 };
+document.querySelector("#randomize").addEventListener("click", newCard);
+// ♦ ♥ ♠ ♣
